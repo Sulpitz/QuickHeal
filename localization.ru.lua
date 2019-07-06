d38246d9cca3080b24daabbb7c8fb0a1444611ad
shikulja <shikulja@rambler.ru>
2019-07-06 23:17:59 +0300

    Create localization.ru.lua
    not Working, need update libs, 
    need add translate PlayerClass , UnitRace

---
+++ localization.ru.lua
@@ -0,0 +1,17 @@
+-- Шаман
+QUICKHEAL_SPELL_LESSER_HEALING_WAVE = "Малая волна исцеления";
+QUICKHEAL_SPELL_HEALING_WAVE = "Волна исцеления";
+
+-- Жрец
+QUICKHEAL_SPELL_LESSER_HEAL = 'Малое исцеление';
+QUICKHEAL_SPELL_HEAL = 'Исцеление';
+QUICKHEAL_SPELL_GREATER_HEAL = 'Великое исцеление';
+QUICKHEAL_SPELL_FLASH_HEAL = 'Быстрое исцеление';
+
+-- Паладин
+QUICKHEAL_SPELL_HOLY_LIGHT = 'Свет небес';
+QUICKHEAL_SPELL_FLASH_OF_LIGHT = 'Вспышка Света';
+
+-- Друид
+QUICKHEAL_SPELL_HEALING_TOUCH = 'Целительное прикосновение';
+QUICKHEAL_SPELL_REGROWTH = 'Восстановление';
